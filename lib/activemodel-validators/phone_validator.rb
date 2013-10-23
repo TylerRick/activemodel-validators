01a38370cb34a5715667061255a023fe9fbca84a
Tyler Rick <tyler.rick@k3integrations.com>
2013-10-22 18:15:09 -0700

    Use errors.add instead of << to be consistent.

--- lib/activemodel-validators/phone_validator.rb
+++ lib/activemodel-validators/phone_validator.rb
@@ -2,7 +2,7 @@ class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
     unless value.to_s.gsub(/[\s-]+/, '').length >= 10
-      record.errors[attribute] << "must have at least 10 digits"
+      record.errors.add attribute, "must have at least 10 digits"
     end
   end
 end
