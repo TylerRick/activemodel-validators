18e09b72be6ba7f20abe938aaadcdd799f5a09d4
Tyler Rick <tyler.rick@k3integrations.com>
2013-10-22 18:59:20 -0700

    Allow min and max to be passed as options to YearValidator

--- lib/activemodel-validators/year_validator.rb
+++ lib/activemodel-validators/year_validator.rb
@@ -3,9 +3,18 @@ module ActiveModel
     class YearValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
         return if value.blank?
-        unless ((min=1700) .. (max=2200)).include? value
+        unless (min .. max).include? value
           record.errors.add attribute, :year_invalid, min: min, max: max
         end
+      end
+
+    private
+      def min
+        options[:min] || 1700
+      end
+
+      def max
+        options[:max] || 2200
       end
     end
   end
