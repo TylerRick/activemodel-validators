97c8dba6630001b4ffffba3ac52bce17fe6da90f
Tyler Rick <tyler.rick@k3integrations.com>
2013-10-22 18:35:26 -0700

    Add :must_have_at_least_10_digits message to en.yml.

--- lib/activemodel-validators/phone_validator.rb
+++ lib/activemodel-validators/phone_validator.rb
@@ -2,7 +2,7 @@ class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
     unless value.to_s.gsub(/[\s-]+/, '').length >= 10
-      record.errors.add attribute, "must have at least 10 digits"
+      record.errors.add attribute, :must_have_at_least_10_digits
     end
   end
 end
