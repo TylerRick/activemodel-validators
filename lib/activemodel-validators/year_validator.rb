f92345b8c59f4e85b107f413f4772ab9595eabe9
Tyler Rick <tyler.rick@k3integrations.com>
2013-10-22 18:50:36 -0700

    Namespace the YearValidator to be like all the other validators.

--- lib/activemodel-validators/year_validator.rb
+++ lib/activemodel-validators/year_validator.rb
@@ -1,8 +1,12 @@
-class YearValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value.blank?
-    unless ((min=1700) .. (max=2200)).include? value
-      record.errors.add attribute, :year_invalid, min: min, max: max
+module ActiveModel
+  module Validations
+    class YearValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        return if value.blank?
+        unless ((min=1700) .. (max=2200)).include? value
+          record.errors.add attribute, :year_invalid, min: min, max: max
+        end
+      end
     end
   end
 end
